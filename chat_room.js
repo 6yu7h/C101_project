6c846f492b7f95cb71106258e0ce0a3171aad1d1
6yu7h <114726087+6yu7h@users.noreply.github.com>
2023-03-08 12:41:07 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA1e5HdCZBcxTuYgFbD3ejcf4uMtm4BteA",
+    authDomain: "project-c101-ed83a.firebaseapp.com",
+    databaseURL: "https://project-c101-ed83a-default-rtdb.firebaseio.com",
+    projectId: "project-c101-ed83a",
+    storageBucket: "project-c101-ed83a.appspot.com",
+    messagingSenderId: "735902270584",
+    appId: "1:735902270584:web:8ade9456f308e46f66998d",
+    measurementId: "G-C8SPCWVPM0"
+  };
+
+  firebase.intializeApp(firebaseConfig);
